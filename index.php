206c30d0165170d5bfdf9c9ed8c13dbb80748a7d
Kzilhoo <adrien.excoffier@gmail.com>
2018-06-28 22:27:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ob_start();
-session_start();
 date_default_timezone_set('Europe/Paris');
 setlocale(LC_ALL, 'fr_FR');
 include("model/connection_pdo.php");
